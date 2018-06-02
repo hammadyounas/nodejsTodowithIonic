fbe6bc8c1d806c37f7cb3158e1969c50eecdd1e3
hammadyounas <m.hammadyounas@outlook.com>
2018-06-02 15:38:41 +0500

    data successfully retrieved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
@@ -21,7 +22,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpClientModule,
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClientModule } from '@angular/common/http';
+import { Http, Headers ,HttpModule } from "@angular/http";
+// import {map} from 'rxjs/Operator'
 // import { Http } from '@angular/http';
 
 
@@ -7,9 +10,25 @@ import { NavController } from 'ionic-angular';
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage { 
+  values =  [];
 
-  constructor(public navCtrl: NavController ) {
+  constructor(public navCtrl: NavController, private http: Http ) {
+    this.getData()
+  }
+  getData(){
+    const headers = new Headers({ "Content-Type": "application/json" });
+    this.http.get('http://localhost:3000/')
+    .subscribe((data:any) =>{
+      this.values  = JSON.parse(data._body);
+      console.log("this.values",this.values);
+    })
+
+  }
+  modify(item){
+
+  }
+  delete(item){
     
   }
 
